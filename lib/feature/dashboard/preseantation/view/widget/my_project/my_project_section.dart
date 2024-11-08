c21473150900e50dae58b6bc838c5734f24f9237
mohamedamr77 <moamr947@gmail.com>
2024-11-08 18:16:18 +0200

    inital my project section

---
+++ lib/feature/dashboard/preseantation/view/widget/my_project/my_project_section.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyProjectSection extends StatelessWidget {
+  const MyProjectSection({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+    children: [
+
+    ],
+    );
+  }
+}
