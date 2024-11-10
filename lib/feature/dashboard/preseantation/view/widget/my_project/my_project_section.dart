e5f6d0acdf410008e438ab578bc691fdd54cb3c4
mohamedamr77 <moamr947@gmail.com>
2024-11-10 03:02:37 +0200

    feat: Add caching to CustomGridViewMyProject to improve performance
    - Implemented AutomaticKeepAliveClientMixin in CachedCustomGridViewMyProject to keep the state alive.
    - Updated MyProjectSection to use CachedCustomGridViewMyProject.

--- lib/feature/dashboard/preseantation/view/widget/my_project/my_project_section.dart
+++ lib/feature/dashboard/preseantation/view/widget/my_project/my_project_section.dart
@@ -3,6 +3,7 @@ import 'package:portfolioapp/core/shared_widget/custom_text_with_desc.dart';
 import 'package:portfolioapp/core/utils/app_text.dart';
 import 'cached_custom_grid_view_my_project.dart';
 
+
 class MyProjectSection extends StatelessWidget {
   const MyProjectSection({super.key});
   @override
