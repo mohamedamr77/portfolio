3d7431eb312e6617ef95410b3e9276dad3d67fcb
mohamedamr77 <moamr947@gmail.com>
2024-11-12 01:16:09 +0200

    make CertificatesSection in mobile

--- lib/feature/dashboard/preseantation/view/widget/certificates/custom_carousel_slider.dart
+++ lib/feature/dashboard/preseantation/view/widget/certificates/custom_carousel_slider.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:portfolioapp/core/extentions/screen_size.dart';
 import 'package:portfolioapp/core/utils/app_color.dart';
+import 'package:portfolioapp/core/utils/size_config.dart';
 import 'package:portfolioapp/feature/dashboard/preseantation/view_model/dashboard_controller/dashboard_state.dart';
 import '../../../../data/model/certificate_list.dart';
 import '../../../view_model/dashboard_controller/dashboard_cubit.dart';
@@ -14,6 +15,9 @@ class CustomCarouselSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cubit = BlocProvider.of<DashboardCubit>(context);
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double aspectRatio = screenWidth < SizeConfig.tabletSize ? 16 / 9 : 1087 / 300;
+
     return BlocBuilder<DashboardCubit, DashboardState>(
       builder: (context, state) {
         return Column(
@@ -22,15 +26,15 @@ class CustomCarouselSlider extends StatelessWidget {
               itemCount: certificateList.length,
               itemBuilder:
                   (BuildContext context, int itemIndex, int pageViewIndex) =>
-                      CarouselSliderItem(
-                itemIndex: itemIndex,
-                certificateModel: certificateList[itemIndex],
-              ),
+                  CarouselSliderItem(
+                    itemIndex: itemIndex,
+                    certificateModel: certificateList[itemIndex],
+                  ),
               options: CarouselOptions(
                 autoPlayCurve: Curves.fastOutSlowIn,
                 autoPlay: true,
-                viewportFraction: 0.45,
-                aspectRatio: 1087 / 300,
+                viewportFraction: 0.9,
+                aspectRatio: aspectRatio,
                 initialPage: 0,
                 onPageChanged: (value, v) {
                   BlocProvider.of<DashboardCubit>(context)
@@ -44,7 +48,7 @@ class CustomCarouselSlider extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: List.generate(
                 certificateList.length,
-                (index) {
+                    (index) {
                   return Container(
                     margin: const EdgeInsets.all(3),
                     height: 0.015.h,
