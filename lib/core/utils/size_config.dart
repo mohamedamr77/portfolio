abf447b8dca5def1968077ea797fe67c54312158
mohamedamr77 <moamr947@gmail.com>
2024-11-12 03:51:39 +0200

    3r

--- lib/core/utils/size_config.dart
+++ lib/core/utils/size_config.dart
@@ -1,5 +1,5 @@
 class SizeConfig {
   static const double desktopSize = 1200;
   static const double tabletSize = 900;
-  static const double mobileSize = 600;
+  static const double mobileSize = 502;
 }
--- lib/feature/dashboard/preseantation/view/widget/my_project/custom_grid_view_my_project.dart
+++ lib/feature/dashboard/preseantation/view/widget/my_project/custom_grid_view_my_project.dart
@@ -44,17 +44,45 @@ int crossAxisCount(BuildContext context) {
 }
 
 double childAspectRatio(BuildContext context) {
-  if (MediaQuery.sizeOf(context).width < SizeConfig.mobileSize) {
+  double width = MediaQuery.sizeOf(context).width;
+
+  //  400 and 470
+  if (width <= 400) {
+    return 1.29;
+
+    // Width between 470 and 500
+  } else if (width > 400 && width < 470) {
+    return 1.4;
+    // Width between 500 and mobile size (typically 500 to < 684)
+  }
+
+  else if (width >= 470 && width < SizeConfig.mobileSize) {
     return 1.7;
-  } else if (MediaQuery.sizeOf(context).width < SizeConfig.tabletSize &&
-      MediaQuery.sizeOf(context).width > 684) {
+
+    // Width between 500 and mobile size (typically 500 to < 684)
+  } else if (width < SizeConfig.mobileSize) {
+    return 1.7;
+
+    // Width between 684 and tablet size (usually up to 900)
+  } else if (width < SizeConfig.tabletSize && width >= 684) {
     return 1.2;
-  } else if (MediaQuery.sizeOf(context).width < SizeConfig.tabletSize &&
-      (MediaQuery.sizeOf(context).width >= SizeConfig.mobileSize)) {
+
+    // Width between 600 and 684
+  } else if (width < SizeConfig.tabletSize && width >= 600) {
     return 1.04;
-  } else if (MediaQuery.sizeOf(context).width < SizeConfig.desktopSize) {
+
+    // Width between mobile size (500) and 600
+  } else if (width < SizeConfig.tabletSize && width >= SizeConfig.mobileSize) {
+    return 0.8;
+
+    // Width between tablet size (900) and desktop size (typically 1200)
+  } else if (width < SizeConfig.desktopSize) {
     return 1.02;
+
+    // Width greater than or equal to desktop size (1200 and above)
   } else {
     return 1.01;
   }
 }
+
+
