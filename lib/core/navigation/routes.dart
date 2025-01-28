a09d978601ccd1f2f69607d2ab08449fb8bcc67f
mohamedamr77 <moamr947@gmail.com>
2025-01-28 21:24:52 +0200

    delete details screen

--- lib/core/navigation/routes.dart
+++ lib/core/navigation/routes.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-
-import '../../feature/project_details/presentation/view/project_details_screen.dart';
 import '../../feature/protfolio/preseantation/view/protfolio_view.dart';
 
 class AppRouter {
   static Map<String, Widget Function(dynamic)> routes = {
     PortfolioScreen.id: (context) => const PortfolioScreen(),
-    ProjectDetailsScreen.id: (context) => const ProjectDetailsScreen(),
-
   };
 }
